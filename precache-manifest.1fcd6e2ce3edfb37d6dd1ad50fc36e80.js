9912e97c8576b91ce17349393e4d94b212dd3ba4
Juho <s6heju04@gmail.com>
2020-02-03 16:25:24 +0200

    Updates

---
+++ precache-manifest.1fcd6e2ce3edfb37d6dd1ad50fc36e80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797643e5261938cefc2aae3f62b38c5c",
+    "url": "/liana-task/index.html"
+  },
+  {
+    "revision": "2b919e560ea70ba95b10",
+    "url": "/liana-task/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a36e496aa09e8f9358aa",
+    "url": "/liana-task/static/css/main.1e7adf91.chunk.css"
+  },
+  {
+    "revision": "2b919e560ea70ba95b10",
+    "url": "/liana-task/static/js/2.aa15f60f.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/liana-task/static/js/2.aa15f60f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36e496aa09e8f9358aa",
+    "url": "/liana-task/static/js/main.d2f3029e.chunk.js"
+  },
+  {
+    "revision": "63602e9a35d473c866c4",
+    "url": "/liana-task/static/js/runtime-main.37c236f9.js"
+  },
+  {
+    "revision": "4bd36fab57f0707eb6f77803fd1ff4fc",
+    "url": "/liana-task/static/media/bannerImage.4bd36fab.jpg"
+  },
+  {
+    "revision": "283cfea2e3cae40a2536e190e13995f6",
+    "url": "/liana-task/static/media/boximage1.283cfea2.jpg"
+  },
+  {
+    "revision": "f8f90565a1c872964ab5eb5892b0e78a",
+    "url": "/liana-task/static/media/liana-technologies-logo.f8f90565.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/liana-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/liana-task/precache-manifest.1fcd6e2ce3edfb37d6dd1ad50fc36e80.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/liana-task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
