da489575e5e818ba806267b2bea4bc0e6ba3ef9e
Juho <s6heju04@gmail.com>
2020-02-29 15:35:02 +0200

    changed project name

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const Banner = () => {
-    return ( <div>
-    <div style={{position:"absolute", left:"10px", top:"120px", color:"white"}}>
-    <div><h1>Software fueling digital marketing</h1></div>
+    return ( <div className="Monke">
+    <div style={{position:"absolute", left:"50%", right:"50%", top:"120px", color:"white", width:"200px"}}>
+    <div><h1 style={{fontSize:"55px"}}>Software fueling digital marketing</h1></div>
     <Button variant="success">Learn more</Button>
     </div>
         <img src={bannerImage} width="100%" alt="Liana Banner"/>
